a4d7c99722a2e909fe622e9a448fa726b1e930c5
Y0URII <dmollimard@gmail.com>
2022-01-22 18:13:27 +0100

    Inistialisation de la liste des utilisateurs, Modification du model, ajoute de la liste dans la home page

--- TeamCreator/src/app/user.ts
+++ TeamCreator/src/app/user.ts
@@ -1,7 +1,33 @@
+export const userList: User[] = [
+  { id: 1, name: 'marion' },
+  { id: 2, name: 'issiah' },
+  { id: 3, name: 'vincent' },
+  { id: 4, name: 'damien' },
+];
+
 export class User {
 
-  constructor(
-    public name: string
-  ) {  }
+  id: number;
+  name: string | undefined;
+
+  constructor(name: string) {
+    // Vérification si l'utilisateur exist
+    userList.forEach((user) =>{
+      if(name.toLowerCase() === user.name){
+        this.id = user.id;
+        this.name = user.name;
+      }
+    });
+
+    // Nouvel utilisateur
+    // @ts-ignore
+    if(this.id == null){
+        this.id = userList.length + 1;
+        this.name = name.toLowerCase();
+        userList.push(this);
+    }
+  }
+
+
 
 }
