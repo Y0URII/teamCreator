f6058b66a0e10342d247afc10210c41c08ecb8e9
SANHNV <45290025+SANHNV@users.noreply.github.com>
2022-01-22 20:28:21 +0100

    fix routing links

--- TeamCreator/src/app/app.module.ts
+++ TeamCreator/src/app/app.module.ts
@@ -5,16 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {CommonModule} from '@angular/common';
-import { AuthComponent } from './Views/auth/auth/auth.component';
-import { DisconnectBtnComponent } from './Views/disconnect-btn/disconnect-btn/disconnect-btn.component';
-import { HomeComponent } from './Views/home/home/home.component';
+import { AuthComponent } from './Views/auth/auth.component';
+import { DisconnectBtnComponent } from './Views/disconnect-btn/disconnect-btn.component';
+import { HomeComponent } from './Views/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
     HomeComponent,
-    DisconnectBtnComponent,
+    DisconnectBtnComponent
   ],
   imports: [
     BrowserModule,
