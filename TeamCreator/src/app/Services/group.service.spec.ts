7b791f4e3aa13609d7a73a3246f915455132e6e5
Vincent Rodriguez <vins_380@hotmail.fr>
2022-01-24 23:44:04 +0100

    code smells

--- TeamCreator/src/app/Services/group.service.spec.ts
+++ TeamCreator/src/app/Services/group.service.spec.ts
@@ -1,42 +1,42 @@
 import { TestBed } from '@angular/core/testing';
 import { GroupService } from './group.service';
-import {Group, groupList} from "../Models/group";
-import {User, userList} from "../Models/user";
-import {UserService} from "./user.service";
-import {GroupConfigService} from "./group-config.service";
-import {HttpClient} from "@angular/common/http";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { groupList } from '../Models/group';
+import { UserService } from './user.service';
+import { GroupConfigService } from './group-config.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('GroupService', () => {
   let service: GroupService;
   let httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
   let groupListTest = groupList;
 
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [GroupService]
+      providers: [GroupService],
     });
-    service = new GroupService(new GroupConfigService(httpClientSpy),new UserService(httpClientSpy), httpClientSpy);
+    service = new GroupService(
+      new GroupConfigService(httpClientSpy),
+      new UserService(httpClientSpy),
+      httpClientSpy
+    );
   });
 
   // Test getGroupbyId
-  it("should return the first group", () => {
+  it('should return the first group', () => {
     service.groupList = groupList;
-    expect(service.getGroupbyId(1)).toBe(groupListTest[0]);//Then
+    expect(service.getGroupbyId(1)).toBe(groupListTest[0]); //Then
   });
 
   // Test getGroupbyId
-  it("should return undefined", () => {
+  it('should return undefined', () => {
     service.groupList = groupList;
-    expect(service.getGroupbyId(0)).toBeUndefined();//Then
+    expect(service.getGroupbyId(0)).toBeUndefined(); //Then
   });
 
   // Test getGroupbyId
-  it("should return false", () => {
+  it('should return false', () => {
     service.groupList = [];
-    expect(service.getGroupbyId(0)).toBeFalse();//Then
+    expect(service.getGroupbyId(0)).toBeFalse(); //Then
   });
-
 });
--- TeamCreator/src/app/Services/user.service.spec.ts
+++ TeamCreator/src/app/Services/user.service.spec.ts
@@ -1,58 +1,50 @@
 import { TestBed } from '@angular/core/testing';
-
 import { UserService } from './user.service';
-import {GroupService} from "./group.service";
-import {GroupConfigService} from "./group-config.service";
-import {User, userList} from "../Models/user";
-import { HttpClientModule } from '@angular/common/http';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {Group, groupList} from "../Models/group";
+import { User, userList } from '../Models/user';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('UserService', () => {
   let service: UserService;
   let httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
   let userListTest = userList;
 
-
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [UserService]
+      providers: [UserService],
     });
     service = new UserService(httpClientSpy);
   });
-
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
   // Test AddUser with a new user
-  it("should insert a new user", () => {
-    let user = new User("test");// Given
-    service.addUser(user);//When
-    expect(userListTest).toContain(user);//Then
+  it('should insert a new user', () => {
+    let user = new User('test'); // Given
+    service.addUser(user); //When
+    expect(userListTest).toContain(user); //Then
   });
 
   // Test removeUser
-  it("should remove a existing user", () => {
-    let user = new User('marion');// Given
-    service.removeUser(user);//When
-    expect(userListTest).not.toContain(user);//Then
+  it('should remove a existing user', () => {
+    let user = new User('marion'); // Given
+    service.removeUser(user); //When
+    expect(userListTest).not.toContain(user); //Then
   });
 
   // Test setGroupToUser
-  it("should set a group to user", () => {
-    let user = new User('userTest');// Given
-    service.userList.push(user);// Given
-    service.setGroupToUser(user, 7);//When
-    expect(user.groupId).toEqual(7);//Then
+  it('should set a group to user', () => {
+    let user = new User('userTest'); // Given
+    service.userList.push(user); // Given
+    service.setGroupToUser(user, 7); //When
+    expect(user.groupId).toEqual(7); //Then
   });
 
   // Test getUserbyName
-  it("should return a user with the name asked", () => {
+  it('should return a user with the name asked', () => {
     let userReturn = service.getUserbyName('marion');
-    expect(userReturn).toBe(service.userList[0]);//Then
+    expect(userReturn).toBe(service.userList[0]); //Then
   });
-
 });
