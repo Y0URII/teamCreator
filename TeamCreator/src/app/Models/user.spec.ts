4abf747256ae2259eaebfa7a81d980d0379a34ab
Y0URII <dmollimard@gmail.com>
2022-01-24 21:45:36 +0100

    Ajout des TU des services et des models

--- TeamCreator/src/app/Models/user.spec.ts
+++ TeamCreator/src/app/Models/user.spec.ts
@@ -1,7 +1,21 @@
 import { User } from './user';
 
 describe('User', () => {
+  beforeEach(() => {});
+
   it('should create an instance', () => {
+    // @ts-ignore
     expect(new User()).toBeTruthy();
   });
+
+  // Test GetUser with exist user
+  it("should return a user", () => {
+    expect(User.getUser(1)).toEqual({ id: 1, name: 'marion', groupId: -1 });
+  });
+
+  // Test GetUser with unexist user
+  it("should return a undefined", () => {
+    expect(User.getUser(9)).toBeUndefined();
+  });
+
 });
